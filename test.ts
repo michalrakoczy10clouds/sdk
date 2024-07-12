2486ea8594109a73a1b49889a08d8a3017fb25cc
Michał Mach <vipmachu@gmail.com>
2024-07-12 12:21:01 +0200

    test tile and vscode debug config

---
+++ test.ts
@@ -0,0 +1,146 @@
+require("dotenv").config();
+
+import {
+  Wallet,
+  CrossClient,
+  Exchange,
+  Network,
+  utils,
+  types,
+  assets,
+  events,
+  constants,
+} from "./src";
+import { PublicKey, Connection, Keypair } from "@solana/web3.js";
+import fetch from "node-fetch";
+import * as bip39 from "bip39";
+import { HDKey } from "micro-ed25519-hdkey";
+import { Asset } from "./src/constants";
+// import { Wallet } from "@coral-xyz/anchor";
+
+const NETWORK_URL = process.env["network_url"]!;
+const SERVER_URL = process.env["server_url"];
+const STARTING_BALANCE = 5_000;
+
+async function exchangeCallback(
+  asset: constants.Asset,
+  _eventType: events.EventType,
+  _data: any
+) {
+  console.log(`[ExchangeCallback] Asset=${assets.assetToName(asset)}`);
+}
+
+async function clientCallback(
+  asset: constants.Asset,
+  _eventType: events.EventType,
+  _data: any
+) {
+  console.log(`[ClientCallback] Asset=${assets.assetToName(asset)}`);
+}
+
+async function main() {
+  // Generate a new keypair for wallet otherwise load from a private key.
+  // const userKey = Keypair.generate();
+
+  // console.log("process.env.MNEMONIC", process.env.MNEMONIC);
+
+  const seed = bip39.mnemonicToSeedSync(process.env.MNEMONIC!, "");
+  const hd = HDKey.fromMasterSeed(seed.toString("hex"));
+  const path = `m/44'/501'/0'/0'`;
+  const keypair = Keypair.fromSeed(hd.derive(path).privateKey);
+  const userKey = keypair;
+  console.log(`${path} => ${keypair.publicKey.toBase58()}`);
+
+  // Loads the private key in .env
+  // const userKey = Keypair.fromSecretKey(
+  //   new Uint8Array(JSON.parse(Buffer.from(process.env.private_key!).toString()))
+  // );
+
+  const wallet = new Wallet(userKey);
+
+  // Create a solana web3 connection to devnet.
+  const connection: Connection = new Connection(NETWORK_URL, "confirmed");
+
+  // Airdropping SOL.
+  // await connection.requestAirdrop(wallet.publicKey, 100_000_000);
+
+  let loadExchangeConfig = types.defaultLoadExchangeConfig(
+    Network.DEVNET,
+    connection,
+    utils.defaultCommitment(),
+    0,
+    true,
+    undefined,
+    undefined,
+    undefined,
+    [Asset.SOL]
+  );
+
+  // Optionally add a throttle for startup load to prevent rate-limiting on free-tier RPCs
+  // loadExchangeConfig.throttleMs = 1000;
+
+  // await fetch(`${SERVER_URL}/faucet/USDC`, {
+  //   method: "post",
+  //   body: JSON.stringify({
+  //     key: wallet.publicKey.toString(),
+  //     amount: 10_000,
+  //   }),
+  //   headers: { "Content-Type": "application/json" },
+  // });
+
+  await Exchange.load(loadExchangeConfig, wallet, exchangeCallback);
+
+  Exchange.getAllSubExchanges().forEach(async (se) => {
+    await se.updatePerpSerumMarketIfNeeded(0);
+  });
+
+  const client = await CrossClient.load(
+    connection,
+    wallet,
+    undefined
+    // , clientCallback
+  );
+
+  let tradingAsset = constants.Asset.APT;
+
+  await client.deposit(utils.convertDecimalToNativeInteger(STARTING_BALANCE));
+
+  utils.displayState();
+
+  // Show current orderbook for a market.
+  console.log(`${tradingAsset} Market orderbook:`);
+  console.log(Exchange.getOrderbook(tradingAsset));
+
+  // Place bid orders
+  await client.placeOrder(
+    tradingAsset,
+    utils.convertDecimalToNativeInteger(0.1),
+    utils.convertDecimalToNativeLotSize(2),
+    types.Side.BID,
+    { tifOptions: {}, orderType: types.OrderType.LIMIT } // Extra optional parameters
+  );
+
+  // Allow orderbook to update (it uses a websocket subscription)
+  await utils.sleep(1000);
+
+  // See our order in the orderbook.
+  console.log(`${tradingAsset} Market orderbook after our order:`);
+  console.log(Exchange.getOrderbook(tradingAsset));
+
+  // See our positions
+  await client.updateState();
+  console.log(
+    `${tradingAsset} positions: ${client.getPositions(tradingAsset)}`
+  );
+
+  // Check mark prices for the product.
+  console.log(
+    `${tradingAsset} Mark price: ${Exchange.getMarkPrice(tradingAsset)}`
+  );
+
+  // Calculate user account state.
+  let accountState = client.getAccountState();
+  console.log(`${tradingAsset} Account state:`, accountState);
+}
+
+main().catch(console.error.bind(console));
